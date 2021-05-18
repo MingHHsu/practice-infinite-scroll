ccfed5d69581eec1f879793ab595b00317eef857
MingHHsu <ming.hs.hsu@gmail.com>
2021-05-19 00:31:57 +0800

    add axiosObservable

---
+++ src/store/apis/axiosObservable.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { Observable } from 'rxjs';
+
+export default ({
+  url,
+  method = 'get',
+  baseURL,
+  headers = {},
+  data = {},
+  params = {},
+}) => new Observable(async (observer) => {
+  try {
+    const res = await axios({
+      url,
+      method,
+      baseURL,
+      headers,
+      data,
+      params,
+    });
+    observer.next(res);
+    observer.complete();
+  } catch (error) {
+    observer.error();
+  }
+});
