f02570dc1c48b066c0b788ddbedf753bce1fcaac
MingHHsu <ming.hs.hsu@gmail.com>
2021-05-19 21:45:57 +0800

    add material ui and normalize.css

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import configureStore, { history } from 'store';
 import Routes from 'routes';
+import 'normalize.css';
 
 const store = configureStore();
 
