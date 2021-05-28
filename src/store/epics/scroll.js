42788c092f9e3efa4858b5bce51658b0fd275311
MingHHsu <ming.hs.hsu@gmail.com>
2021-05-28 14:03:29 +0800

    update scroll epic

--- src/store/epics/scroll.js
+++ src/store/epics/scroll.js
@@ -1,45 +1,40 @@
 import { ofType } from 'redux-observable';
-import { fromEvent, of, merge } from 'rxjs';
 import {
-  mergeMap, takeUntil, map, filter,
+  of,
+  iif,
+  EMPTY,
+  fromEvent,
+} from 'rxjs';
+import {
+  map,
+  filter,
+  concatMap,
+  takeUntil,
 } from 'rxjs/operators';
 import {
   startScroll,
   stopScroll,
 } from 'store/slices/scroll';
 
-function detectScrollToBottom$({
-  handleRequest,
-}) {
-  return of(document.body.clientHeight === window.pageYOffset + window.innerHeight).pipe(
-    filter((scrollToBottom) => scrollToBottom),
-    map(handleRequest),
-  );
-}
-
 function detectStopScroll$({
   handleRequest,
   scenicSpotType,
   state$,
 }) {
-  return of(state$.value.scenicSpot[scenicSpotType].noMoreScenicSpot).pipe(
-    mergeMap((noMoreScenicSpot) => merge(
-      of(noMoreScenicSpot).pipe(
-        filter(() => noMoreScenicSpot),
-        map(stopScroll),
-      ),
-      of(noMoreScenicSpot).pipe(
-        filter(() => !noMoreScenicSpot),
-        mergeMap(() => detectScrollToBottom$({ handleRequest })),
-      ),
-    )),
+  return iif(
+    () => state$.value.scenicSpot[scenicSpotType].noMoreScenicSpot,
+    EMPTY.pipe(map(stopScroll)),
+    of(document.body.clientHeight === window.pageYOffset + window.innerHeight).pipe(
+      filter((scrollToBottom) => scrollToBottom),
+      map(handleRequest),
+    ),
   );
 }
 
 export default (action$, state$) => action$.pipe(
   ofType(startScroll),
-  mergeMap((action) => fromEvent(document, 'scroll').pipe(
-    mergeMap(() => detectStopScroll$({ ...action.payload, state$ })),
+  concatMap((action) => fromEvent(document, 'scroll').pipe(
+    concatMap(() => detectStopScroll$({ state$, ...action.payload })),
     takeUntil(action$.pipe(ofType(stopScroll))),
   )),
 );
